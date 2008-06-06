879b712731e4a16002876d2f7b09b0831662b523
Ray Krueger <raykrueger@gmail.com>
2008-06-06 14:24:15 +0000

    don't call key.hashCode twice...
    git-svn-id: http://hibernate-memcached.googlecode.com/svn/trunk/hibernate-memcached@73 c703cd00-4848-0410-a446-1f3cffe0bef0

--- src/main/java/com/googlecode/hibernate/memcached/HashCodeKeyStrategy.java
+++ src/main/java/com/googlecode/hibernate/memcached/HashCodeKeyStrategy.java
@@ -9,7 +9,7 @@ public class HashCodeKeyStrategy extends AbstractKeyStrategy {
 
     protected String transformKeyObject(Object key) {
         int hashCode = key.hashCode();
-        log.debug("Transformed key [{}] to hashCode [{}]", key, key.hashCode());
+        log.debug("Transformed key [{}] to hashCode [{}]", key, hashCode);
         return String.valueOf(hashCode);
     }
 
